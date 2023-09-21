20340d1b228b6f131716189403685e7e4a2b810c
namratha-p-s <namratha.psom.97@gmail.com>
2023-09-21 13:17:29 -0600

    Initial Commit

---
+++ Wikipedia/WikipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikipediaApp.swift
+//  Wikipedia
+//
+//  Created by Namratha P Somachudan on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct WikipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
